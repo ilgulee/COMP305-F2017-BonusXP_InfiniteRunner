f7d486afd4dbac982cca58d8fbdafec0151f5640
ilgulee <iglee2000ca@gmail.com>
2017-12-02 08:49:42 -0500

    Init
    Set Player, Run and Jump

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class PlayerController : MonoBehaviour
+    {
+        public float MoveSpeed;
+
+        public float JumpForce;
+
+        private Rigidbody2D _playeRigidbody;
+        // Use this for initialization
+        void Start ()
+        {
+            _playeRigidbody = GetComponent<Rigidbody2D>();
+        }
+	
+        // Update is called once per frame
+        void Update () {
+		    _playeRigidbody.velocity=new Vector2(MoveSpeed,_playeRigidbody.velocity.y);
+            if (Input.GetKeyDown(KeyCode.Space)||Input.GetKeyDown(0))
+            {
+                _playeRigidbody.velocity=new Vector2(_playeRigidbody.velocity.x,JumpForce);
+            }
+        }
+    }
+}
