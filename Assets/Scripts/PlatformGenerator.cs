0159cd7b4310ef3c973ea49401d1bb5a171d5623
ilgulee <iglee2000ca@gmail.com>
2017-12-03 16:39:57 -0500

    Auto generating Platforms

---
+++ Assets/Scripts/PlatformGenerator.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class PlatformGenerator : MonoBehaviour
+    {
+
+        public GameObject ThePlatform;
+
+        public Transform GenerationPoint;
+
+        public float DistanceBetween;
+
+        private float _platformWidth;
+
+        // Use this for initialization
+        void Start ()
+        {
+            _platformWidth = ThePlatform.GetComponent<BoxCollider2D>().size.x;
+        }
+	
+        // Update is called once per frame
+        void Update () {
+            if (transform.position.x < GenerationPoint.position.x)
+            {
+                transform.position=new Vector3(transform.position.x+_platformWidth+DistanceBetween,transform.position.y,transform.position.z);
+                Instantiate(ThePlatform, transform.position, transform.rotation);
+            }
+        }
+    }
+}
